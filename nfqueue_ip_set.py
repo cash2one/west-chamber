f134eadd9f11389b3361a6e4ee03a995d39cb70b
Fen Qin <fqrouter@gmail.com>
2013-04-15 11:29:39 +0800

    ipset with get_mark

--- nfqueue_ip_set.py
+++ nfqueue_ip_set.py
@@ -10,15 +10,14 @@ raw_socket.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
 
 def handle_two_side_traffic(nfqueue_element):
     try:
+        print('mark: %s' % nfqueue_element.get_mark())
+        if nfqueue_element.get_mark():
+            nfqueue_element.accept()
+            return
         ip_packet = dpkt.ip.IP(nfqueue_element.get_payload())
         dst = socket.inet_ntoa(ip_packet.dst)
-        print(dst)
-        if '106.186.19.85' == dst:
-            print('matched')
-            nfqueue_element.set_mark(1)
-            nfqueue_element.repeat()
-        else:
-            nfqueue_element.accept()
+        nfqueue_element.set_mark(1)
+        nfqueue_element.repeat()
     except:
         traceback.print_exc()
         nfqueue_element.accept()
@@ -30,15 +29,13 @@ nfqueue.bind(0, handle_two_side_traffic)
 
 def clean_up(*args):
     # will be called twice, don't know why
-    subprocess.call('iptables -D OUTPUT -m mark --mark 0x1 -j DROP', shell=True)
-    subprocess.call('iptables -D OUTPUT -m mark ! --mark 0x1 -p tcp -j QUEUE', shell=True)
+    subprocess.call('iptables -D OUTPUT -p tcp -j QUEUE', shell=True)
 
 
 signal.signal(signal.SIGINT, clean_up)
 
 try:
-    subprocess.call('iptables -I OUTPUT -m mark --mark 0x1 -j DROP', shell=True)
-    subprocess.call('iptables -I OUTPUT -m mark ! --mark 0x1 -p tcp -j QUEUE', shell=True)
+    subprocess.call('iptables -I OUTPUT -p tcp -j QUEUE', shell=True)
     print('running..')
     nfqueue.run()
 except KeyboardInterrupt:
