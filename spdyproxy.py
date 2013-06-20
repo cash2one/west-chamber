1856448e6325b402127d72e59419d0e7a6e2d40d
Fen Qin <fqrouter@gmail.com>
2013-06-20 16:44:53 +0800

    working spdy spike

--- spdyproxy.py
+++ spdyproxy.py
@@ -1,30 +1,65 @@
 import socket
 import ssl
 import spdylay
-import M2Crypto
+import select
+import time
+
+
+def on_stream_close_cb(session, stream_id, status_code):
+    print('close', stream_id)
+
+
+def on_ctrl_recv_cb(session, frame):
+    if frame.frame_type == spdylay.SYN_REPLY:
+        print(frame.nv)
+
+
+def on_data_chunk_recv_cb(session, flags, stream_id, data):
+    print(data)
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('198.52.103.140', 443))
-def callback(*args, **kwargs):
-    print('!!!')
-    return 0
 try:
-    ssl_ctx = M2Crypto.SSL.Context(protocol='sslv3')
-    ssl_ctx.set_next_protocol_callback(callback)
-    sock = M2Crypto.SSL.Connection(ssl_ctx, sock)
-    sock.setup_ssl()
-    sock.connect_ssl()
+    sock = ssl.wrap_socket(sock, npn_protocols=['spdy/3'])
+    sock.connect(('198.52.103.140', 443))
+    print(sock.selected_protocol())
     sock.setblocking(False)
-    session = spdylay.Session(spdylay.CLIENT, spdylay.PROTO_SPDY3)
+
+    def send_cb(session, data):
+        print('send cb')
+        return sock.send(data)
+
+    session = spdylay.Session(
+        spdylay.CLIENT, spdylay.PROTO_SPDY3,
+        send_cb=send_cb,
+        on_ctrl_recv_cb=on_ctrl_recv_cb,
+        on_data_chunk_recv_cb=on_data_chunk_recv_cb,
+        on_stream_close_cb=on_stream_close_cb)
     session.submit_request(0,
                            [(':method', 'GET'),
-                            (':scheme', 'https'),
+                            (':scheme', 'http'),
                             (':path', '/'),
                             (':version', 'HTTP/1.1'),
-                            (':host', 'baidu.com:80'),
+                            (':host', 'www.baidu.com'),
                             ('accept', '*/*'),
                             ('user-agent', 'python-spdylay')])
-    session.send()
+    while True:
+        if session.want_read():
+            print('read')
+            try:
+                data = sock.recv(4096)
+                if data:
+                    session.recv(data)
+                else:
+                    break
+            except ssl.SSLError:
+                select.select([sock], [], [])
+        if session.want_write():
+            print('write')
+            try:
+                session.send()
+            except ssl.SSLError:
+                select.select([], [sock], [])
 finally:
     sock.shutdown(socket.SHUT_RDWR)
     sock.close()
